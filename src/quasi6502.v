3d2448f9872a744e5c398573b61bea38e2267818
Pavel Kretov <firegurafiku@gmail.com>
2017-11-03 14:34:40 +0300

    module quasi6502: create stub
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

---
+++ src/quasi6502.v
@@ -0,0 +1,16 @@
+
+module quasi6502
+   (input  wire         reset,
+    output wire         ready,
+    input  wire         clk,
+    input  wire         irq,
+    input  wire         nmi,
+    output wire         sync,
+    output wire         busRequestWrite,
+    output wire [15:0]  busRqAddress,
+    input  wire  [7:0]  busDataIn,
+    output wire  [7:0]  busDataOut);
+
+    // STUB.
+endmodule
+
